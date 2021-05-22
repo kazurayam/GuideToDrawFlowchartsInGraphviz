5ec1c734adbaca4cd83c31135abe2666289fea35
kazurayam <kazuaki.urayama@gmail.com>
2021-05-22 11:06:44 +0900

    add the cases #2, #3, #4

--- draw.sh
+++ draw.sh
@@ -1,1 +1,8 @@
-dot -Tpng 1_flowchart.dot -o tmp/1_flowchart.png
+#dot -Tpng 1_a_first_attempt.dot -o tmp/1_a_first_attempt.png
+
+#dot -Tpng 2_taking_shape.dot -o tmp/2_taking_shape.png
+
+#dot -Tpng 3_winners_to_the_left_please.dot -o tmp/3_winners_to_the_left_please.png
+
+dot -Tpng 4_no_breathing_room.dot -o tmp/4_no_breathing_room.png
+
